1184ec0b205fe7b038e9d3b5ff608434eabe7e31
Nolan Holden <nolanholden@users.noreply.github.com>
2018-04-03 14:45:38 -0400

    fix typedefs

--- rcs/types.hh
+++ rcs/types.hh
@@ -4,22 +4,31 @@
 #include <type_traits>
 #include <limits>
 
+#ifndef RCS_USE_FAST_INT
+# define RCS_USE_FAST_INT 0
+#endif
+
+#ifndef RCS_ASSERT_IEC559_FLOATING_POINTS
+# define RCS_ASSERT_IEC559_FLOATING_POINTS 1
+#endif
+
+
 #ifndef RCS_BYTE_TYPE
-#  include <cstdint>
-#  ifdef RCS_USE_FAST_INT
-#    define RCS_BYTE_TYPE std::uint_fast8_t
-#  else
-#    define RCS_BYTE_TYPE std::uint8_t
-#  endif // RCS_USE_FAST_INT
+# include <cstdint>
+# if RCS_USE_FAST_INT
+#   define RCS_BYTE_TYPE std::uint_fast8_t
+# else
+#   define RCS_BYTE_TYPE std::uint8_t
+# endif // RCS_USE_FAST_INT
 #endif // !RCS_BYTE_TYPE
 
 #ifndef RCS_FLOAT32_TYPE
-#  define RCS_FLOAT32_TYPE float
-#endif // !RCS_FLOAT32_TYPE
+# define RCS_FLOAT32_TYPE float
+#endif
 
 #ifndef RCS_FLOAT64_TYPE
-#  define RCS_FLOAT64_TYPE double
-#endif // !RCS_FLOAT64_TYPE
+# define RCS_FLOAT64_TYPE double
+#endif
 
 namespace rcs {
 
@@ -33,16 +42,21 @@ static_assert(std::is_unsigned<byte_t>::value, "rcs assumes byte_t is unsigned")
 /**
 * @breif System-wide "float" (32-bit floating point) type.
 */
-using f32_t = float;
-static_assert(sizeof(f32_t) == 4, "f32_t must be 32 bits (4 destination) wide");
-static_assert(std::numeric_limits<f32_t>::is_iec559, "must be IEEE 754 floating point");
+using f32_t = RCS_FLOAT32_TYPE;
+static_assert(sizeof(f32_t) == 4, "f32_t must be 32 bits (4 bytes) wide");
+#if RCS_ASSERT_IEC559_FLOATING_POINTS
+static_assert(std::numeric_limits<f32_t>::is_iec559, "must be IEC-559 floating point");
+#endif
+
 
 /**
 * @breif System-wide "double" (64-bit floating point) type.
 */
-using f64_t = double;
-static_assert(sizeof(f64_t) == 8, "f64_t must be 64 bits (8 destination) wide");
-static_assert(std::numeric_limits<f32_t>::is_iec559, "must be IEEE 754 floating point");
+using f64_t = RCS_FLOAT64_TYPE;
+static_assert(sizeof(f64_t) == 8, "f64_t must be 64 bits (8 bytes) wide");
+#if RCS_ASSERT_IEC559_FLOATING_POINTS
+static_assert(std::numeric_limits<f32_t>::is_iec559, "must be IEC-559 floating point");
+#endif
 
 } // namespace rcs
 
